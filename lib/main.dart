ce9b7b21c30e58a8d393f1f5f72ed2130a450dec
pavly samir <bavlysamir33@gmail.com>
2026-05-25 03:22:54 +0300

    feat: enforce portrait orientation across Android and iOS platforms

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lowgos_app/core/cashe/cache_helper.dart';
@@ -14,7 +15,7 @@ void main() async {
   await CacheHelper.init();
   setupInjection();
   await ScreenUtil.ensureScreenSize();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   Bloc.observer = SimpleBlocObserver();
   runApp(const LawgosApp());
 }
-
